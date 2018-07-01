400482971e71a894b179671c382ab779e2daebd0
ciemic <michal.cieslar@gmail.com>
2018-07-01 12:11:18 +0200

    Loop added

--- Hello.cpp
+++ Hello.cpp
@@ -1,9 +1,12 @@
-#include <stdio.h>
 #include <iostream>
 
 int main()
 {
     std::cout << "Hello world"<< std::endl;
     std::cout << "Hello Coders Schoole" << std::<endl;
+    for ( int i =0; i < 10; i++)
+    {
+	std::cout << i;
+    }
     return 0;
 }
