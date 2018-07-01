0bb8c4255089f76735ec0ab0b811fc6b916a4dc5
ciemic <michal.cieslar@gmail.com>
2018-07-01 12:18:14 +0200

    Hello master

--- Hello.cpp
+++ Hello.cpp
@@ -4,6 +4,7 @@
 int main()
 {
     std::cout << "Hello world"<< std::endl;
-    std::cout << "Hello Coders Schoole" << std::<endl;
+    std::cout << "Hello Coders Schoole" << std::endl;
+    std::cout << "Hello Master" << std::endl;
     return 0;
 }
